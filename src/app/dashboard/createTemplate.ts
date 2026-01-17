b1bfc0bb42baf175d5dc027bbaf4cb4dc4c66c53
charansaipasupuleti <charanpasupuleti08@gmail.com>
2026-01-17 14:38:49 +0530

    version 6.0

--- src/app/dashboard/createTemplate.ts
+++ src/app/dashboard/createTemplate.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-create-template',
   imports: [
     CommonModule, 
-    MatDialogModule
+    MatDialogModule,
+    FormsModule
   ],
   templateUrl: './createTemplate.html',
   styleUrls: ['./createTemplate.css']
@@ -14,7 +16,38 @@ export class CreateTemplateComponent {
       templateName: string = 'UK-MHRA-01';
   region: string = 'UK';
 
-  onModify() { console.log('Modify clicked'); }
-  onSave() { console.log('Save clicked'); }
-  onCancel() { console.log('Cancel clicked'); }
+  onModify() { 
+    console.log('Modify clicked');
+
+   }
+  onSave() {
+     console.log('Save clicked');
+     }
+
+  showStructure:boolean=false;
+
+  onCreateStructure(){
+    this.showStructure=true;
+  }
+  treeData = [
+    {
+      label: 'Application ID',
+      expanded: true,
+      children: [
+        {
+          label: 'Sequence 0001',
+          expanded: true,
+          children: [
+            { label: 'm1', children: [{ label: 'us' }] },
+            { label: 'm2', children: [{ label: '23-qos' }, { label: '27-clin-sum' }] },
+            { label: 'm3' },
+            { label: 'm4' },
+            { label: 'm5' },
+            { label: 'util' }
+          ]
+        }
+      ]
+    }
+  ];
+
 }
